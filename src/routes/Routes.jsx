8eebb3822438cf2d2bdea825f925f560e854a0d6
Dany Leonardo Martinez Murcia <dany.martinez@norsoft.com.co>
2025-05-18 20:13:35 -0500

    SE realiza nuevamente el commit subiendo ajustes o desarrollos de Vista de Detalle de Libro de Relatos de Papel.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router'
 import Home from '@src/pages/home//Home'
+import BookDetail from '@src/pages/bookdetail/BookDetail'
 import RootLayout from './RootLayout'
 
 const routes = [
@@ -10,6 +11,10 @@ const routes = [
       {
         path: '/home',
         element: <Home />,
+      },
+      {
+        path: '/book/:id',
+        element: <BookDetail />,
       }
     ],
   },
